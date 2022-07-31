9985ffe7e7be21771d40cbcd1114487f5f8b143a
Marine Mellado <marine.mellado@gmail.com>
2022-07-31 22:30:07 +0200

    text treatment function as module

--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ const { default: mongoose } = require("mongoose");
 
 const tesseract = require("node-tesseract-ocr");
 const vision = require("@google-cloud/vision");
+const treatText = require("./text_treatment_function")
 
 //-------ROUTE TESSERACT - POST
 router.post("/tesseract", async function (req, res, next) {
@@ -26,7 +27,6 @@ router.post("/url-scrapper", async function (req, res, next) {
 
 
 //---Test route tesseract
-
 //ne pas oublier d'installer l'engine tesseract => brew install tesseract
 //+packages langues => brew install tesseract-lang (attention, tres long )
 router.get("/tesseract", async function (req, res, next) {
@@ -35,9 +35,8 @@ router.get("/tesseract", async function (req, res, next) {
     // 	"https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg"
     // );
 
-
     // var image = "https://picturetherecipe.com/wp-content/uploads/2013/07/Picture-The-Recipe-Tips-Muffin-tin-for-stuffed-veggies.jpg"
-    var image = "./tests/images/ochazuke_9.png"
+    var image = "./tests/images/ok/ochazuke_9.png"
 
     const config = {
         lang: "fra",
@@ -48,7 +47,9 @@ router.get("/tesseract", async function (req, res, next) {
         .recognize(image, config)
         .then((text) => {
             console.log("Result:", text)
-            res.json({ text });
+            console.log(treatText(text))
+            var resultObj = treatText(text)
+            res.json({ resultObj });
         })
         .catch((error) => {
             console.log(error.message)
@@ -56,6 +57,5 @@ router.get("/tesseract", async function (req, res, next) {
         })
 
 });
-
 
 module.exports = router;
