9e640e1d9b02f49cf131eee537aa47aee579abb0
Dylan <pinpon@ordi-pinpon.local>
2022-08-01 01:25:07 +0200

    Recipes and Users models completion

--- models/recipes.js
+++ models/recipes.js
@@ -1,13 +1,33 @@
-var mongoose = require('mongoose')
+var mongoose = require("mongoose");
 
+var ingredientSchema = mongoose.Schema({
+	name: String,
+	quantity: Number,
+});
+
+var tagSchema = mongoose.Schema({
+	name: String,
+});
+
+var commentSchema = mongoose.Schema({
+	author: { type: mongoose.Schema.Types.ObjectId, ref: "addresses" },
+	date: Date,
+	content: String,
+});
 
 var recipeSchema = mongoose.Schema({
-    name: String,
-    img: String,
-    ingredients: Array,
-    process: String
-  });
-  
-  module.exports = mongoose.model('recipes', recipeSchema);
+	name: String,
+	ingredients: ingredientSchema,
+	directions: String,
+	servings: Number, //pour combien de personnes
+	prepTime: Number, //en minutes
+	cookTime: Number, //en minutes
+	tags: tagSchema, //tableau de strings
+	author: { type: mongoose.Schema.Types.ObjectId, ref: "addresses" }, //personne qui l'a importé ou créé dans hungrybook, et pas le cuisinier qui l'a inventé
+	image: String, //url
+	comments: commentSchema,
+	likeCount: Number, //nb de likes
+	privateStatus: Boolean, //apparait dans le feed ou pas
+});
 
-
+module.exports = mongoose.model("recipes", recipeSchema);
