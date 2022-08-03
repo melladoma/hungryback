62b5cd51966db5082f28e39c5088c62557c51de5
Dylan <pinpon@ordi-pinpon.local>
2022-08-03 11:57:44 +0200

    acheve recherche feed

--- routes/search.js
+++ routes/search.js
@@ -4,6 +4,38 @@ var router = express.Router();
 var recipeModel = require("../models/recipes");
 var userModel = require("../models/users");
 
+router.post("/initial-fetch-myrecipes", async function (req, res, next) {
+	console.log(req.body.token);
+	var myAccount = await userModel
+		.findOne({ token: req.body.token })
+		.populate("addedRecipes")
+		.populate("likedRecipes");
+
+	res.json({
+		addedRecipes: myAccount.addedRecipes,
+		likedRecipes: myAccount.likedRecipes,
+	});
+});
+
+module.exports = router;
+
+router.post("/initial-fetch-feedrecipes", async function (req, res, next) {
+	var allRecipes = await recipeModel.find();
+	console.log(allPublicRecipes)
+	var allPublicRecipes = allRecipes.filter((x) => x.privateStatus === false);
+
+	console.log(allPublicRecipes)
+
+	res.json({ allPublicRecipes });
+});
+
+module.exports = router;
+
+
+
+
+//des aides mémoires que je garde pour l'instant:
+
 //---- ROUTE RECHERCHE PAR TAGS - GET onPress
 //DONNEES d'ENTREE: tableau de chips/tags selected
 //DONNEES DE SORTIE:tableau de recettes qui contiennent ces tags [{title,ingredients,direction,persons,cookingTime,prepTime,tags,author,picture,comments,likeCount,privateStatus}]
@@ -23,7 +55,6 @@ var userModel = require("../models/users");
 
 	res.json({ recipes });
 });  */
-
 
 //---- ROUTE RECHERCHE TEXTUELLE - POST sur TouchableOpacity Magnify
 //DONNEES d'ENTREE: req.body.searchItem (string)
@@ -58,50 +89,4 @@ var userModel = require("../models/users");
 }); */
 
 
-/* router.post("/search-input-myrecipes", async function (req, res, next) {
-	//recettes feed
-	console.log(req.body.input)
-	var recipesByName = []
-	var recipesByIngredients = []
-	var recipesByDirections = []
-	if (req.body.input.length === 0) {
-		recipesByName = await recipeModel.find()
-	} else {
-		
-		listAuthor = (await userModel.find()).map(x=>x.username)
-		newListAuthor = listAuthor.filter(x=>x.indexOf(req.body.input) === 0 ) //filtrer si ca correspond à la lettre
-		console.log(listAuthor)
-		
 
-		
-		let regex = new RegExp(req.body.input, 'i')
-	recipesByName = await recipeModel.find({ name: reg });
-	console.log(recipesByName)
-	recipesByIngredients = await recipeModel.find({ ingredients: { "$all": reg } });
-	recipesByDirections = await recipeModel.find({ directions: reg });
-	}
-	
-
-	res.json({ recipesByName, listAuthor: JSON.stringify(newListAuthor), recipesByDirections });
-}); */
-
-
-
-router.post("/initial-fetch-myrecipes", async function (req, res, next) {
-	console.log(req.body.token)
-	var myAccount = await userModel.findOne({token : req.body.token}).populate('addedRecipes').populate('likedRecipes')
-	
-	res.json({ addedRecipes : myAccount.addedRecipes, likedRecipes: myAccount.likedRecipes });
-});
-
-module.exports = router;
-
-
-router.post("/initial-fetch-feedrecipes", async function (req, res, next) {
-	console.log(req.body.token)
-	var myAccount = await userModel.findOne({token : req.body.token}).populate('addedRecipes').populate('likedRecipes')
-	
-	res.json({ addedRecipes : myAccount.addedRecipes, likedRecipes: myAccount.likedRecipes });
-});
-
-module.exports = router;
